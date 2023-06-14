e00b16f17a0b0dbd33c3efea92d3cd7b0008f70d
ANDRESROJAS00 <andre.rojasm@duocuc.cl>
2023-06-14 15:43:31 -0400

    #funciones actualizado

--- funciones.py
+++ funciones.py
@@ -1,5 +1,15 @@
-#
+def calcular_iva(precio_objeto):
+    iva = precio_objeto * 0.19
+    return iva
 
-print('funciones')
+def descuento(precio_producto, descuento_aplicar):
+    des = precio_producto * (descuento_aplicar/100)
+    total_descuento = precio_producto - des
 
-print('Pruea de comit 2')
+def calcular_imc(peso, estatura):
+    imc = peso / (estatura ** 2)
+    return imc
+
+
+
+
